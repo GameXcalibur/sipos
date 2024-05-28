c5ca47d5d5c34ce86b6911c91cfd6627d7d01951
kylemhansen <kylehansen75@gmail.com>
2024-05-28 22:16:18 +0200

    Test FIX

---
+++ resources/views/header.blade.php
@@ -0,0 +1,23 @@
+
+
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+
+</head>
+<body>
+<div class="row" style="margin-top: 25px;">
+                    <div class="col-xs-12">
+                            <p style="font-style: italic;text-align: center">The SILUX CONTROL ProEM system not only meets the requirements of the latest BS 5266-1-2016 standard
+but also ensures all compliance testing is carried out in accordance with BS EN 50172:2004 and BS 5266-8:2004 and BS 5266-1:2016
+The PRO-EM is updated every hour with status information which can be viewed at any time
+Weekly Test: Flash Test 1 Minute; Monthly Test: 30 Minute; Bi-Annual Test: 1.5 hours; Annual Test: 3 hours</p>
+                        </div>
+                        <div class="col-xs-12">
+                            <p style="font-style: italic;text-align: center">Silux Control UK Ltd © 2024.</p>
+                        </div>
+                    </div>
+</body>
+</html>
