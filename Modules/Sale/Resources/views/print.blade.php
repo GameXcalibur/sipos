b24409915f24cea61cd2baf7039da896e5b35ed3
Kyle Hansen <kylehansen@Kyles-MacBook-Pro.local>
2024-06-04 17:14:07 +0200

    CHNAGES

--- Modules/Sale/Resources/views/print.blade.php
+++ Modules/Sale/Resources/views/print.blade.php
@@ -65,7 +65,7 @@ body {
 
                     <div class="table-responsive-sm" style="margin-top: 30px; background-color: transparent !important;">
 
-                        <table class="table table-striped" style="background: url('{{ public_path('images/opacity.png') }}') !important; background-size: 100% !important;  background-position: center !important;" >
+                        <table class="table table-striped" style="background-color: transparent !important;" >
                             <thead>
                             <tr>
                                 <th class="align-middle">Device</th>
@@ -76,7 +76,7 @@ body {
                             </thead>
                             
     
-                            <tbody >
+                            <tbody style="">
                             @foreach($tests as $test)
                                 <tr style="background-color: none !important; ">
                                     <td class="align-middle" style="background-color: none !important;">
