ea1f50ebc3327ea3ed1a12249e985b56fa2785d0
Kyle Hansen <kylehansen@Kyles-MBP.lan>
2024-06-11 11:21:39 +0200

    CHNAGES

--- resources/views/notifications.blade.php
+++ resources/views/notifications.blade.php
@@ -59,6 +59,77 @@
 
     <hr>
 
+    <div class="row">
+        <div class="col-md-12">
+            <div class="row" >
+                <div class="col-md-8" >
+
+                    <input type='email' name='email' placeholder='' style='width: 80%;' class="form-control"></input>
+
+                </div>
+                <div class="col-md-4" >
+
+                    <select style='width: 100%;' class="form-control">
+                        <option>Once / Day</option>
+                        <option>Once / Week</option>
+                        <option>Once / Month</option>
+                        <option>Once / Year</option>
+
+                    </select>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <hr>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="row" >
+                <div class="col-md-8" >
+
+                    <input type='email' name='email' placeholder='' style='width: 80%;' class="form-control"></input>
+
+                </div>
+                <div class="col-md-4" >
+
+                    <select style='width: 100%;' class="form-control">
+                        <option>Once / Day</option>
+                        <option>Once / Week</option>
+                        <option>Once / Month</option>
+                        <option>Once / Year</option>
+
+                    </select>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <hr>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="row" >
+                <div class="col-md-8" >
+
+                    <input type='email' name='email' placeholder='' style='width: 80%;' class="form-control"></input>
+
+                </div>
+                <div class="col-md-4" >
+
+                    <select style='width: 100%;' class="form-control">
+                        <option>Once / Day</option>
+                        <option>Once / Week</option>
+                        <option>Once / Month</option>
+                        <option>Once / Year</option>
+
+                    </select>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <hr>
+
 
 
 
